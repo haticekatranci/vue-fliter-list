261fb0ecd127ed992a8db6a5463b11a305f3c5ac
hatice <hatice@hotmail.com>
2022-12-12 00:22:37 +0300

    filter list

---
+++ filter-list/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ filter-list/src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+<div id="app">
+  <input
+      type="text"
+      placeholder="Search"
+      class="search-input"
+      v-model="searchValue"
+  />
+  <div class="users-container" v-if="usersList.length"
+  v-for="(user, index) in usersList"
+  :key="index"
+  >
+  {{ index+1 }}. {{ user.name }}
+  </div>
+  <div v-else >
+    No users found for this search: {{ searchValue }}
+  </div>
+
+</div>
+</template>
+
+
+<script>
+export default {
+  name : 'App',
+  components:{},
+  data: () => ({
+    searchValue:'',
+    users: [
+      {
+        name:'Jhon',
+        age:'45',
+      }, {
+        name:'Doe',
+        age:'35',
+      },
+      {
+        name:'Basit',
+        age:'32',
+      }, {
+        name:'Jason',
+        age:'38',
+      },
+
+
+
+    ]
+
+  }),
+  computed:{
+    usersList(){
+      if(this.searchValue.trim().length > 0){
+        return this.users.filter((user) => user.name.toLowerCase().includes(this.searchValue.trim().toLowerCase()))
+      }
+      return this.users
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+.search-input {
+  width: 200px;
+  height: 30px;
+  border-radius: 5px;
+}
+
+</style>
